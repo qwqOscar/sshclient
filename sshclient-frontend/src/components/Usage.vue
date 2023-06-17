118acdb7db633d570a15f1ef260997575ebcfbe5
stay00 <1687559852@qq.com>
2023-06-17 17:21:03 +0800

    modify: 修改主页内容

--- sshclient-frontend/src/components/Usage.vue
+++ sshclient-frontend/src/components/Usage.vue
@@ -1,80 +1,63 @@
 <template>
-    <div class="documentation">
-      <h1>使用说明</h1>
+  <div class="documentation">
+    <h1>信息安全原理课程课程设计</h1>
+    <h2>2053925 万唯循 1953246 方若愚</h2>
+    <h2>问题重述</h2>
+    <el-card>
+      <h3>考虑若干智能手机为用户终端，数据存储和web服务部署在一个linux服务器上，服务器与终端间应用层至少采用TLS保护，设计一个基于ssh对该服务器的远程配置和管理的原型系统。</h3>
+    </el-card>
+
+    <h2>工作总述</h2>
+    <el-card>
+      <h3>
+        在上述问题的背景下，我们开发了一款桌面应用，前端使用了vite+vue3+electron的框架，中间件使用了python的paramiko库，paramiko库支持基于ssh的和sftp的远程连接，并且可以模拟终端命令行的交互，同时我们使用websocket作为前端和中间件的连接，值得一提的是两者的websocket连接的url为localhost，在本地运行
+      </h3>
+    </el-card>
+  </div>
+</template>
   
-      <h2>步骤 1</h2>
-      <el-card>
-        <h3>安装</h3>
-        <p>使用以下命令安装依赖：</p>
-        <pre><code>npm install element-plus</code></pre>
-      </el-card>
+<script>
+export default {
+  name: 'Documentation',
+  data() {
+    return {
+      inputValue: '',
+    };
+  },
+};
+</script>
   
-      <h2>步骤 2</h2>
-      <el-card>
-        <h3>导入</h3>
-        <p>在你的项目中导入 Element Plus：</p>
-        <pre><code>import ElementPlus from 'element-plus';
-  import 'element-plus/lib/theme-chalk/index.css';
-  
-  createApp(App).use(ElementPlus).mount('#app');</code></pre>
-      </el-card>
-  
-      <h2>步骤 3</h2>
-      <el-card>
-        <h3>使用组件</h3>
-        <p>在需要使用的组件中添加 Element Plus 的组件标签：</p>
-        <pre><code>&lt;template>
-    &lt;div>
-      &lt;el-button type="primary">主要按钮&lt;/el-button>
-      &lt;el-input v-model="inputValue" placeholder="请输入内容">&lt;/el-input>
-    &lt;/div>
-  &lt;/template></code></pre>
-      </el-card>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'Documentation',
-    data() {
-      return {
-        inputValue: '',
-      };
-    },
-  };
-  </script>
-  
-  <style scoped>
-  .documentation {
-    padding: 20px;
-  }
-  
-  h1 {
-    font-size: 24px;
-    margin-bottom: 10px;
-  }
-  
-  h2 {
-    font-size: 20px;
-    margin-top: 20px;
-    margin-bottom: 10px;
-  }
-  
-  h3 {
-    font-size: 16px;
-    margin-bottom: 10px;
-  }
-  
-  pre {
-    background-color: #f5f7fa;
-    padding: 10px;
-    margin: 10px 0;
-    border-radius: 4px;
-    overflow-x: auto;
-  }
-  
-  code {
-    font-size: 14px;
-  }
-  </style>
+<style scoped>
+.documentation {
+  padding: 20px;
+}
+
+h1 {
+  font-size: 24px;
+  margin-bottom: 10px;
+}
+
+h2 {
+  font-size: 20px;
+  margin-top: 20px;
+  margin-bottom: 10px;
+}
+
+h3 {
+  font-size: 16px;
+  margin-bottom: 10px;
+}
+
+pre {
+  background-color: #f5f7fa;
+  padding: 10px;
+  margin: 10px 0;
+  border-radius: 4px;
+  overflow-x: auto;
+}
+
+code {
+  font-size: 14px;
+}
+</style>
   
